32556079e6a989b5232d6f5e8072990ce68b5954
Rushiraj Brahmbhatt <rushibrahmbhatt33@gmail.com>
2024-11-19 22:24:01 +0530

    Cors fix

--- src/api/helpers.ts
+++ src/api/helpers.ts
@@ -13,6 +13,7 @@ export async function apiFetcher(url: string, config?: RequestInit) {
 export async function fetchAPI<T>(url: string, config?: RequestInit): Promise<T | null> {
   const headers = new Headers();
   headers.append("Content-Type", "application/json");
+  headers.append("Access-Control-Allow-Origin", "*");
   const response = await fetch(API_BASE_URL_SERVER + url, { headers, ...config });
   const data = await response.json();
 
